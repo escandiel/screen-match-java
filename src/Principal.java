4299460782a2da63b2a3a00bbc301f0a58f8adc1
Victor Escandiel <joaoescandielb@gmail.com>
2023-10-19 13:30:49 -0300

    new class: Principal

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,10 @@
 public class Principal {
     public static void main(String[] args){
-
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O poderoso chefao";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+        System.out.println(meuFilme.nome);
+        System.out.println(meuFilme.anoDeLancamento);
     }
 }
